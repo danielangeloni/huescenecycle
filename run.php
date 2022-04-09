eb4cbe20847dfc900c8593f99e1c447f7481bd72
Daniel A <84752451+danielangeloni@users.noreply.github.com>
2022-04-09 20:44:11 +1000

    Update script to utilise API v2
    I have just discovered there is a way to call the dynamic scenes without using the hue lab alternatives. The way to do this is to use the v2 API over HTTPS. This commit updates the script to always play the dynamic pallette version of a random scene, unless it could not find a dynamic scene; to which it will fallback on the static one. Nonetheless, the script still maintains its stance of being 'dynamic', in the sense that user changes to scenes in the Hue app will be carried over in this script. Only change is it will play dynamic scenes now instead of static ones.

--- run.php
+++ run.php
@@ -11,12 +11,19 @@
 
         function getRequest($url) {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'http://'.$this->hueIP.'/api/'.$this->hueUsername.''.$url.'');
+            $headers = array();
+            if (strpos($url, 'clip/v2') !== false) {
+                $headers[] = 'hue-application-key: '.$this->hueUsername.'';
+                curl_setopt($ch, CURLOPT_URL, 'https://'.$this->hueIP.''.$url.'');
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            } else {
+                $headers[] = '';
+                curl_setopt($ch, CURLOPT_URL, 'http://'.$this->hueIP.'/api/'.$this->hueUsername.''.$url.'');
+            }
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-            $headers = array();
-            $headers[] = '';
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             $result = curl_exec($ch);
             if (curl_errno($ch)) {
                 echo 'Error:' . curl_error($ch);
@@ -29,15 +36,24 @@
 
         function putRequest($url, $field) {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'http://'.$this->hueIP.'/api/'.$this->hueUsername.''.$url.'');
+            $headers = array();
+            if (strpos($url, 'clip/v2') !== false) {
+                $headers[] = 'hue-application-key: '.$this->hueUsername.'';
+                curl_setopt($ch, CURLOPT_URL, 'https://'.$this->hueIP.''.$url.'');
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            } else {
+                $headers[] = '';
+                curl_setopt($ch, CURLOPT_URL, 'http://'.$this->hueIP.'/api/'.$this->hueUsername.''.$url.'');
+            }
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_TIMEOUT, 5);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
             curl_setopt($ch, CURLOPT_POSTFIELDS, $field);
-            $headers = array();
-            $headers[] = '';
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            $result = curl_exec($ch);            
+            
+            $result = curl_exec($ch);    
+            if (strpos($result, 'errors":[]') !== false) $result .= "success";
             echo "putRequest: " . $result;
             if (curl_errno($ch) || strpos($result, 'success') == false) {
                 echo 'Error:' . curl_error($ch);
@@ -70,11 +86,30 @@
         }
 
         function setScene($sceneID) {
-            echo "Setting Scene: " . $sceneID;
             if ($sceneID == "colorloop") {
+                echo "Setting Scene: " . $sceneID;
                 $this->putRequest("/groups/$this->group/action", '{"effect":"colorloop", "on": true}');
             } else {
-                $this->putRequest("/groups/$this->group/action", '{"scene": "'.$sceneID.'"}');
+                echo "Scene is a dynamic. Searching for dynamic scene. ";
+                $notFound = false;
+                $scenesDynamic = json_decode($this->getRequest("/clip/v2/resource/scene"), true);
+                foreach($scenesDynamic as $key => $scene) {
+                    foreach($scene as $obj) {
+                        if ($obj["id_v1"] == '/scenes/'.$sceneID.'') {
+                            $notFound = true;
+                            $sceneIDNew = $obj["id"];
+                            echo "Setting dyanmic scene: " . $sceneIDNew;
+                            $this->putRequest("/clip/v2/resource/scene/$sceneIDNew", '{"recall": {"action": "dynamic_palette"}}');
+                            break;
+                        }                       
+                    }
+                }
+
+                if (!$notFound) {
+                    echo 'No dynamic scene found (possible error, or scene is a storageScene scene (hue labs). ';
+                    echo "Setting original scene: " . $sceneID;
+                    $this->putRequest("/groups/$this->group/action", '{"scene": "'.$sceneID.'"}');
+                }
                 $this->putRequest("/groups/$this->group/action", '{"bri": 179}');
             }
         }
@@ -86,5 +121,4 @@
     $hueControl->fetchScenes();
     $hueControl->setScene($hueControl->getRandomScene());
 
-    
 ?>
